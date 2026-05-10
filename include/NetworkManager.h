053973569d9e3e2d1bae81084761c000de025872
abcsnowwolf <liuyf1117@hotmail.com>
2026-05-10 18:07:16 +0800

    Fix nonblocking BLE WiFi startup

--- include/NetworkManager.h
+++ include/NetworkManager.h
@@ -31,6 +31,12 @@ class NetworkManager {
 public:
     NetworkManager();
     void begin();
+    void loop();
+    bool connect(const String& ssid, const String& password);
+    bool isConnected() const;
+    String getSSID() const;
+    String getIPAddress() const;
+    NetworkState getState() const;
 
 private:
     static constexpr uint32_t CONNECT_TIMEOUT = 15000; // 15 seconds
@@ -39,7 +45,12 @@ private:
 
     uint8_t connectAttempts;
     uint32_t lastConnectAttempt;
+    NetworkState state;
+    String requestedSSID;
+    String requestedPassword;
+    bool statusPrinted;
 
+    void startConnection(const char* ssid = nullptr, const char* password = nullptr);
     void setupWebServer();
     
     String generateUniqueSSID();
